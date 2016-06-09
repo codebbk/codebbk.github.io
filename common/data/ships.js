e762f1ce189f963e35754f217e83f6b4954b6d1d
optckr <rostory@naver.com>
2016-06-09 21:42:35 +0900

    Update ships.js

--- common/data/ships.js
+++ common/data/ships.js
@@ -37,7 +37,7 @@ window.ships = [
         thumb: 'ship_0003_c.png',
         description: '파티의 체력X1.5, 사격유형 캐릭터의 공격력 100증가',
         atkStatic: function(p) {
-            return !p.unit.class.has('Shooter') ? 0 :
+            return !p.unit.class.has('사격') ? 0 :
                 [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ][p.boatLevel -1];
         },
         hp: function(p) {
@@ -58,15 +58,15 @@ window.ships = [
     },
 
     { // 4
-        name: 'Coffin Boat',
+        name: '관선',
         thumb: 'ship_0005_c.png',
         description: '참격유형 캐릭터의 공격과 체력 X1.5 , 선장의 회복력 700 감소',
         atk: function(p) {
-            return !p.unit.class.has('Slasher') ? 1 :
+            return !p.unit.class.has('참격') ? 1 :
                 [ 1.1, 1.1, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return !p.unit.class.has('Slasher') ? 1 :
+            return !p.unit.class.has('참격') ? 1 :
                 [ 1.1, 1.1, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
         rcvStatic: function(p) {
@@ -79,14 +79,14 @@ window.ships = [
         thumb: 'ship_0006_c.png',
         description: '받는 데미지 10% 감소, 타격유형 캐릭터의 공격 100증가',
         atkStatic: function(p) {
-            return !p.unit.class.has('Striker') ? 0 : [ 0, 0, 0, 0, 0, 0, 50, 50, 50, 100 ][p.boatLevel -1];
+            return !p.unit.class.has('타격') ? 0 : [ 0, 0, 0, 0, 0, 0, 50, 50, 50, 100 ][p.boatLevel -1];
         }
     },
 
     { // 6
-        name: 'Merry Go Flying',
+        name: '고잉 메리호 비행모델',
         thumb: 'ship_0007_c.png',
-        description: 'Boosts ATK by 1.2x, recovers 350 HP at the end of the turn',
+        description: '일당의 공격력 1.2배 증가, 턴 종료 후 체력을 350 회복한다.',
         atk: function(p) {
             return p.boatLevel < 7 ? 1 : [ 1.1, 1.1, 1.1, 1.2 ][p.boatLevel - 7];
         },
@@ -96,9 +96,9 @@ window.ships = [
     },
 
     { // 7
-        name: 'Moby Dick',
+        name: '모비딕',
         thumb: 'ship_0008_c1.png',
-        description: 'Boosts ATK by 1.5x, boosts HP by 1.4x, reduces HP by 50% at the start of the fight',
+        description: '일당의 공격력 1.5배 증가, 체력 1.4배 증가, 전투 사작시 체력이 50% 상태로 된다.',
         atk: function(p) {
             return [ 1.2, 1.2, 1.25, 1.3, 1.3, 1.35, 1.4, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
@@ -108,9 +108,9 @@ window.ships = [
     },
 
     { // 8
-        name: 'Big Top',
+        name: '빅 탑',
         thumb: 'ship_0009_c1.png',
-        description: 'Boosts ATK of characters with 20 cost or less by 1.5x, boosts HP of characters with 4 stars and below by 1.4x',
+        description: '코스트 20이하 캐릭터의 공격력이 1.5배 증가,  4성 이하 캐릭터의 체력이 1.4배증가',
         atk: function(p) {
             var matching = p.unit.cost <= 15 || (p.unit.cost <= 20 && p.boatLevel >= 6);
             return matching ? [ 1.1, 1.1, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.4, 1.5 ][p.boatLevel - 1] : 1;
@@ -121,105 +121,105 @@ window.ships = [
     },
 
     { // 9
-        name: 'Bezan Black',
+        name: '베잔 블랙',
         thumb: 'ship_0010_c1.png',
-        description: 'Reduces cooldown of all specials by 1 turn at the start of the fight, boosts ATK of QCK characters by 1.4x and their HP by 1.3x',
+        description: '전투 시작시 스킬 1턴 단축, 속 속성 공격력 1.4배, 체력 1.3배 증가',
         atk: function(p) {
-            return p.unit.type != 'QCK' ? 1 : [ 1.1, 1.1, 1.15, 1.2, 1.2, 1.2, 1.25, 1.3, 1.3, 1.4 ][p.boatLevel - 1];
+            return p.unit.type != '속도' ? 1 : [ 1.1, 1.1, 1.15, 1.2, 1.2, 1.2, 1.25, 1.3, 1.3, 1.4 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return p.unit.type != 'QCK' ? 1 : [ 1, 1.1, 1.1, 1.1, 1.15, 1.2, 1.2, 1.2, 1.3, 1.3 ][p.boatLevel - 1];
+            return p.unit.type != '속도' ? 1 : [ 1, 1.1, 1.1, 1.1, 1.15, 1.2, 1.2, 1.2, 1.3, 1.3 ][p.boatLevel - 1];
         }
     },
 
     { // 10
-        name: 'Aokiji\'s Bike',
+        name: '아오키지의 자전거',
         thumb: null,
-        description: 'Boosts ATK and HP of Striker characters by 1.5x, greatly reduces the appearance of RCV orbs',
+        description: '타격타입의 공격력과 체력 1.5배 증가, [고기]슬롯 출현률 현저히 감소',
         atk: function(p) {
-            return !p.unit.class.has('Striker') ? 1 :
+            return !p.unit.class.has('타격') ? 1 :
                 [ 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return !p.unit.class.has('Striker') ? 1 :
+            return !p.unit.class.has('타격') ? 1 :
                 [ 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         }
     },
 
     { // 11
-        name: 'Ace\'s Striker',
+        name: '에이스의 스트라이커',
         thumb: null,
-        description: 'Boosts ATK of Shooter characters by 1.5x and their HP by 1.3x, reduces cooldown of all specials by 1 turn at the start of the fight',
+        description: '사격타입의 공격력 1.5배, 체력 1.3배 증가, 전투 시작전 사격타입 스킬 1턴 단축',
         atk: function(p) {
-            return !p.unit.class.has('Shooter') ? 1 :
+            return !p.unit.class.has('사격') ? 1 :
                 [ 1.2, 1.2, 1.2, 1.25, 1.25, 1.3, 1.3, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return !p.unit.class.has('Shooter') ? 1 :
+            return !p.unit.class.has('사격') ? 1 :
                 [ 1, 1.1, 1.1, 1.1, 1.2, 1.2, 1.25, 1.25, 1.3, 1.3 ][p.boatLevel - 1];
         }
     },
 
     { // 12
-        name: 'Dreadnaught Sabre',
+        name: '드레드노트 샤벨',
         thumb: 'ship_0014_c1.png',
-        description: 'Boosts HP by 1.5x, deals 5,000 typeless damage at the end of the turn',
+        description: '일당의 체력 1.5배 증가, 턴 종료 후 무속성 데미지 5,000',
         hp: function(p) {
             return [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.5][p.boatLevel - 1];
         }
     },
 
     { // 13
-        name: 'Thousand Sunny',
+        name: '사우전드 써니호',
         thumb: 'ship_0013_c.png',
-        description: 'Boosts ATK by 1.5x. Special: deals 50,000 typeless damage to all enemies (cooldown: 15 turns).',
+        description: '일당의 공격력 1.5배, 필살기: 적 전체에 50,000의 무속성 데미지 (쿨타임: 15 턴)',
         atk: function(p) {
             return [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1.5 ][p.boatLevel - 1];
         }
     },
     
     { //14
-        name: 'Kuja Pirate Ship',
+        name: '쿠자 해적선',
         thumb: null,
-        description: 'Boosts ATK of Free Spirit characters by 1.5x and their HP by 1.35x, reduces the HP of everyone else by 99%. Special: heals the crew by 6,500HP (cooldown: 15 turns).',
+        description: '자유타입의 공격력 1.5배, 체력 1.35배 증가, 자유타입외 체력 99% 감소. 필살기: 체력 6,500 회복 (쿨타임: 15 턴).',
         atk: function(p) {
-            return !p.unit.class.has('Free Spirit') ? 1 :
+            return !p.unit.class.has('자유') ? 1 :
             [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.5 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return !p.unit.class.has('Free Spirit') ? 0.01 :
+            return !p.unit.class.has('자유') ? 0.01 :
                 [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.3, 1.35 ][p.boatLevel - 1];
         }
     },
     
     { //15
-        name: 'Ark Maxim',
+        name: '방주 맥심',
         thumb: null,
-        description: 'Boosts ATK of QCK and PSY characters by 1.5x and their HP by 1.2x. Special: Deals 56560 fixed Damage to all enemies (cooldown: 17 Turns)',
+        description: '속 속성, 심 속성 공격력 1.5배, 체력 1.2배 증가. 필살기: 적 전체에 56,560의 고정 데미지 (쿨타임: 17 턴)',
         atk: function(p) {
-            return p.unit.type == "PSY" || p.unit.type == "QCK" ? [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.5 ][p.boatLevel - 1] : 1;
+            return p.unit.type == "마음" || p.unit.type == "속도" ? [ 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.5 ][p.boatLevel - 1] : 1;
         },
         hp: function(p) {
-            return p.unit.type == "PSY" || p.unit.type == "QCK" ? [ 1, 1, 1, 1, 1, 1.1, 1.1, 1.1, 1.1, 1.2 ][p.boatLevel - 1] : 1;
+            return p.unit.type == "마음" || p.unit.type == "속도" ? [ 1, 1, 1, 1, 1, 1.1, 1.1, 1.1, 1.1, 1.2 ][p.boatLevel - 1] : 1;
         }
     },
     
     { // 16
-        name: 'Red Force',
+        name: '레드 포스',
         thumb: null,
-        description: 'Boosts ATK of Cerebral characters by 1.5x and their HP by 1.35x, Increased Orb Rate for Cerebral characters.',
+        description: '박식 타입의 공격력 1.5배, 체력 1.35배 증가 , 자슬롯 출현율 증가.',
         atk: function(p) {
-            return  !p.unit.class.has('Cerebral') ? 1 : [ 1.0, 1.2, 1.2, 1.2, 1.25, 1.3, 1.4, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
+            return  !p.unit.class.has('박식') ? 1 : [ 1.0, 1.2, 1.2, 1.2, 1.25, 1.3, 1.4, 1.4, 1.4, 1.5 ][p.boatLevel - 1];
         },
         hp: function(p) {
-            return !p.unit.class.has('Cerebral') ? 1 : [ 1.1, 1.1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.35 ][p.boatLevel - 1];
+            return !p.unit.class.has('박식') ? 1 : [ 1.1, 1.1, 1.2, 1.2, 1.2, 1.2, 1.2, 1.3, 1.3, 1.35 ][p.boatLevel - 1];
         }
     },
     
     { // 17
-        name: '2nd Anniversary Thousand Sunny',
+        name: '2주년 사우전드 써니호',
         thumb: null,
-        description: 'Boosts ATK by 1.2x.',
+        description: '일당의 공격력 1.2배.',
         atk: function(p) { return 1.2; },
     }
 
